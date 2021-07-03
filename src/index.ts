635195d961f228f9b06e2ff4629a20d8d64e843d
oktheniguess <63266962+oktheniguess@users.noreply.github.com>
2021-07-02 19:10:10 -0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+document.getElementById("app").innerHTML = `
+<h1>eat shit</h1>
+<div>
+ guess what?
+ <div>
+  <a href="https://www.foodservicedirect.com/simplot-cauliflower-florets-32-oz-package-12-packages-per-case-151896.html" target="_blank" rel="noopener noreferrer">suck my balls</a>
+
+</div>
+`;
